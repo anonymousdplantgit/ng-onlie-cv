30be31b8a1647d2b787c4e9a41dcbc3b62cb599f
souhailaitsaid <souhailaitsaid@gmail.com>
2025-09-04 16:04:37 +0200

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,80 +3,17 @@ import {
   importProvidersFrom,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideTranslateService } from '@ngx-translate/core';
+import { routes } from "./app.routes";
+import { provideHttpClient } from "@angular/common/http";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { provideTranslateService } from "@ngx-translate/core";
 // Import all required icons
-import {
-  // Navigation & UI Icons
-  Menu,
-  X,
-  Sun,
-  Moon,
-  Globe,
-  ChevronDown,
-  ArrowRight,
-  ArrowLeft,
-  Check,
-  CheckCircle,
-  Loader2,
-
-  // Automotive & Service Icons
-  Car,
-  Truck,
-  Wrench,
-  Settings,
-  Cog,
-  ScanLine,
-  Zap,
-  ShieldCheck,
-  Building,
-  Cpu,
-
-  // Contact & Communication Icons
-  Phone,
-  Mail,
-  Send,
-  Calendar,
-  Clock,
-  MapPin,
-  Compass,
-
-  // User & Team Icons
-  User,
-  Users,
-  Award,
-  Star,
-  GraduationCap,
-  Heart,
-
-  // Business & Stats Icons
-  Briefcase,
-  Target,
-  TrendingUp,
-  Layers,
-  Database,
-  Link,
-  Brain,
-  Coffee,
-
-  // Alert & Status Icons
-  AlertCircle,
-  Info,
-  Sparkles,
-
-  // Social Media Icons
-  Facebook,
-  Twitter,
-  Linkedin,
-  Instagram, LucideAngularModule, Github
-} from 'lucide-angular';
-
+import { LucideAngularModule } from "lucide-angular";
+import { lucideIcons } from "./lucide-icons";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -86,79 +23,15 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({
-        prefix: '/assets/i18n/',
-        suffix: '.json',
+        prefix: "/assets/i18n/",
+        suffix: ".json",
       }),
-      fallbackLang: 'fr',
-      lang: 'fr',
+      fallbackLang: "en",
+      lang: "en",
     }),
     importProvidersFrom(
       BrowserAnimationsModule,
-      LucideAngularModule.pick({
-        // Navigation & UI Icons
-        Github,
-        Menu,
-        X,
-        Sun,
-        Moon,
-        Globe,
-        ChevronDown,
-        ArrowRight,
-        ArrowLeft,
-        Check,
-        CheckCircle,
-        Loader2,
-
-        // Automotive & Service Icons
-        Car,
-        Truck,
-        Wrench,
-        Settings,
-        Cog,
-        ScanLine,
-        Zap,
-        ShieldCheck,
-        Building,
-        Cpu,
-
-        // Contact & Communication Icons
-        Phone,
-        Mail,
-        Send,
-        Calendar,
-        Clock,
-        MapPin,
-        Compass,
-
-        // User & Team Icons
-        User,
-        Users,
-        Award,
-        Star,
-        GraduationCap,
-        Heart,
-
-        // Business & Stats Icons
-        Briefcase,
-        Target,
-        TrendingUp,
-        Layers,
-        Database,
-        Link,
-        Brain,
-        Coffee,
-
-        // Alert & Status Icons
-        AlertCircle,
-        Info,
-        Sparkles,
-
-        // Social Media Icons
-        Facebook,
-        Twitter,
-        Linkedin,
-        Instagram
-      })
-    )
+      LucideAngularModule.pick(lucideIcons),
+    ),
   ],
 };
